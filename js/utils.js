e73e91564ed88475d7fc75da65c5631143eb2461
GuruHarum <gurutahtaharum@gmail.com>
2026-07-08 11:17:27 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,13 @@
 function formatCurrentDate() {
-
+            const days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+            const months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+            const now = new Date();
+            const day = days[now.getDay()];
+            const date = now.getDate();
+            const month = months[now.getMonth()];
+            const year = now.getFullYear();
+            
+            return `${day}, ${date} ${month} ${year}`;
 }
 
 function getDaysInMonth(year, month) {
